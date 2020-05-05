a6bb5a10db13e9fe6a0d5dc80c67f3fba58073df
softiet <howdie11002@gmail.com>
2020-05-06 02:58:51 +0800

    a

--- src/model_deploy/main.cpp
+++ src/model_deploy/main.cpp
@@ -90,195 +90,103 @@ int main(int argc, char* argv[]) {
 
 
   // Create an area of memory to use for input, output, and intermediate arrays.
-
   // The size of this will depend on the model you're using, and may need to be
-
   // determined by experimentation.
-
   constexpr int kTensorArenaSize = 60 * 1024;
-
   uint8_t tensor_arena[kTensorArenaSize];
-
-
   // Whether we should clear the buffer next time we fetch data
-
   bool should_clear_buffer = false;
-
   bool got_data = false;
-
-
   // The gesture index of the prediction
-
   int gesture_index;
-
-
   // Set up logging.
-
   static tflite::MicroErrorReporter micro_error_reporter;
-
   tflite::ErrorReporter* error_reporter = &micro_error_reporter;
-
-
   // Map the model into a usable data structure. This doesn't involve any
-
   // copying or parsing, it's a very lightweight operation.
-
   const tflite::Model* model = tflite::GetModel(g_magic_wand_model_data);
-
   if (model->version() != TFLITE_SCHEMA_VERSION) {
-
     error_reporter->Report(
-
         "Model provided is schema version %d not equal "
-
         "to supported version %d.",
-
         model->version(), TFLITE_SCHEMA_VERSION);
-
     return -1;
-
   }
 
-
   // Pull in only the operation implementations we need.
-
   // This relies on a complete list of all the ops needed by this graph.
-
   // An easier approach is to just use the AllOpsResolver, but this will
-
   // incur some penalty in code space for op implementations that are not
-
   // needed by this graph.
 
   static tflite::MicroOpResolver<6> micro_op_resolver;
 
   micro_op_resolver.AddBuiltin(
-
       tflite::BuiltinOperator_DEPTHWISE_CONV_2D,
-
       tflite::ops::micro::Register_DEPTHWISE_CONV_2D());
-
   micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_MAX_POOL_2D,
-
                                tflite::ops::micro::Register_MAX_POOL_2D());
-
   micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_CONV_2D,
-
                                tflite::ops::micro::Register_CONV_2D());
-
   micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_FULLY_CONNECTED,
-
                                tflite::ops::micro::Register_FULLY_CONNECTED());
-
   micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_SOFTMAX,
-
                                tflite::ops::micro::Register_SOFTMAX());
-
   micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_RESHAPE,
                                tflite::ops::micro::Register_RESHAPE(),1);
 
   // Build an interpreter to run the model with
 
   static tflite::MicroInterpreter static_interpreter(
-
       model, micro_op_resolver, tensor_arena, kTensorArenaSize, error_reporter);
-
   tflite::MicroInterpreter* interpreter = &static_interpreter;
 
-
   // Allocate memory from the tensor_arena for the model's tensors
-
   interpreter->AllocateTensors();
-
-
   // Obtain pointer to the model's input tensor
-
   TfLiteTensor* model_input = interpreter->input(0);
-
   if ((model_input->dims->size != 4) || (model_input->dims->data[0] != 1) ||
-
       (model_input->dims->data[1] != config.seq_length) ||
-
       (model_input->dims->data[2] != kChannelNumber) ||
-
       (model_input->type != kTfLiteFloat32)) {
-
     error_reporter->Report("Bad input tensor parameters in model");
-
     return -1;
-
   }
 
 
   int input_length = model_input->bytes / sizeof(float);
-
-
   TfLiteStatus setup_status = SetupAccelerometer(error_reporter);
-
   if (setup_status != kTfLiteOk) {
-
     error_reporter->Report("Set up failed\n");
-
     return -1;
-
   }
 
 
   error_reporter->Report("Set up successful...\n");
-
-
   while (true) {
-
-
     // Attempt to read new data from the accelerometer
-
     got_data = ReadAccelerometer(error_reporter, model_input->data.f,
-
                                  input_length, should_clear_buffer);
-
-
     // If there was no new data,
-
     // don't try to clear the buffer again and wait until next time
-
     if (!got_data) {
-
       should_clear_buffer = false;
-
       continue;
-
     }
-
 
     // Run inference, and report any error
-
     TfLiteStatus invoke_status = interpreter->Invoke();
-
     if (invoke_status != kTfLiteOk) {
-
       error_reporter->Report("Invoke failed on index: %d\n", begin_index);
-
       continue;
-
     }
-
-
     // Analyze the results to obtain a prediction
-
     gesture_index = PredictGesture(interpreter->output(0)->data.f);
-
-
     // Clear the buffer next time we read data
-
     should_clear_buffer = gesture_index < label_num;
-
-
     // Produce an output
-
     if (gesture_index < label_num) {
-
       error_reporter->Report(config.output_message[gesture_index]);
-
     }
 
   }
