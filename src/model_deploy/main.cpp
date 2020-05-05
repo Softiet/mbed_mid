dabeabea23767ef6677fc84640d83ac5ec797c6f
softiet <howdie11002@gmail.com>
2020-05-06 03:00:57 +0800

    a

--- src/model_deploy/main.cpp
+++ src/model_deploy/main.cpp
@@ -10,81 +10,67 @@
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/version.h"
-
+#include <mbed.h>
 
 // Return the result of the last prediction
-
 int PredictGesture(float* output) {
-
   // How many times the most recent gesture has been matched in a row
-
   static int continuous_count = 0;
-
   // The result of the last prediction
-
   static int last_predict = -1;
-
-
   // Find whichever output has a probability > 0.8 (they sum to 1)
-
   int this_predict = -1;
 
   for (int i = 0; i < label_num; i++) {
-
     if (output[i] > 0.8) this_predict = i;
-
   }
 
 
   // No gesture was detected above the threshold
 
   if (this_predict == -1) {
-
     continuous_count = 0;
-
     last_predict = label_num;
-
     return label_num;
-
   }
-
 
   if (last_predict == this_predict) {
-
     continuous_count += 1;
-
   } else {
-
     continuous_count = 0;
-
   }
-
   last_predict = this_predict;
 
-
   // If we haven't yet had enough consecutive matches for this gesture,
-
   // report a negative result
 
   if (continuous_count < config.consecutiveInferenceThresholds[this_predict]) {
-
     return label_num;
-
   }
-
   // Otherwise, we've seen a positive result, so clear all our variables
-
   // and report it
-
   continuous_count = 0;
-
   last_predict = -1;
 
-
   return this_predict;
-
 }
 
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalOut led3(LED3);
+
+InterruptIn sw2(SW2);
+
+EventQueue queue(32 * EVENTS_EVENT_SIZE);
+
+Thread t;
+
+bool pause = false;
+
+void Trig_pause() {
+    // Safe to use 'printf' in context of thread 't', while IRQ is not.
+    printf("paused!");
+}
 
 int main(int argc, char* argv[]) {
 
